6d5da00c9ba7395224155a0890b64cf420cd5c27
Soroosh Morshedi <93315815+Sorooshmorshedi@users.noreply.github.com>
2022-05-16 13:55:47 +0430

    ok

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
+      wellcome to my site<br />
+      powered by soroosh
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
       >.
